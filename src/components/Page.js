926d065fb0c464d4c7b86af04362ed4a699fda00
Reggio <contact@graphandco.com>
2020-05-03 12:31:23 +0200

    Remove dependance for useEffect title hook

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,7 +4,7 @@ const Page = (props) => {
     useEffect(() => {
         document.title = `${props.title} | Graph and Co`;
         window.scrollTo(0, 0);
-    }, []);
+    });
     return <>{props.children}</>;
 };
 
